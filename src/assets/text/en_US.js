0812106dd2cf82dd3f1d9554f839b200b326004e
Halfa <85602339+HalfaChink@users.noreply.github.com>
2022-09-29 20:43:18 +0700

    Add files via upload

---
+++ src/assets/text/en_US.js
@@ -0,0 +1,3 @@
+export const MSG_NO_ITEMS = 'Tidak ada item.';
+export const INFO_SHORTCUT_KEYS = 'Tekan `/` untuk mencari dan `N` untuk membuat item baru.';
+export const INFO_CANCEL_SHORTCUT_KEY = 'Tekan `ESC` untuk cancel';
